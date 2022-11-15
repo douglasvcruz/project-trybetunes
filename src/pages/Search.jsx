868d80dd988a51a1ee1338864a41a5b643cae30e
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-15 18:27:46 -0300

    5º Requisito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,40 @@ import { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    name: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { name } = this.state;
+    const num = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ name.length < num }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
