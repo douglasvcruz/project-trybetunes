9617c91f75aabd2812eb5c26726c8c1515d84072
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-16 12:09:06 -0300

    9º e 10º Requisito

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,26 +1,78 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 export default class MusicCard extends Component {
+  state = {
+    loading: false,
+    checked: false,
+  };
+
+  componentDidMount() {
+    this.songs();
+  }
+
+  songs = async () => {
+    const { musica } = this.props;
+    const som = await getFavoriteSongs();
+
+    som.forEach((element) => {
+      if (element.trackId === musica.trackId) {
+        this.setState({
+          checked: true,
+        });
+      }
+    });
+  };
+
+  songAdd = async () => {
+    const { musica } = this.props;
+    this.setState({
+      loading: true,
+      checked: false,
+    });
+    await addSong(musica);
+
+    this.setState({
+      checked: true,
+      loading: false,
+    });
+  };
+
   render() {
     const { musica } = this.props;
+    const { checked, loading } = this.state;
     return (
       <div>
-        { musica.map((a) => (
-          <div key={ a.trackId }>
-            <p>{a.trackName}</p>
-            <audio data-testid="audio-component" src={ a.previewUrl } controls>
-              <track kind="captions" />
-              {`O seu navegador não suporta o elemento ${a.previewUrl}`}
-              <code>audio</code>
-            </audio>
-          </div>
-        ))}
+        <div>
+          <p>{musica.trackName}</p>
+          <audio data-testid="audio-component" src={ musica.previewUrl } controls>
+            <track kind="captions" />
+            {`O seu navegador não suporta o elemento ${musica.previewUrl}`}
+            <code>audio</code>
+          </audio>
+          { loading ? <Loading /> : (
+            <label htmlFor="checkbox">
+              <input
+                name="trackId"
+                data-testid={ `checkbox-music-${musica.trackId}` }
+                type="checkbox"
+                checked={ checked }
+                onChange={ this.songAdd }
+              />
+            </label>
+          )}
+        </div>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  musica: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  musica: PropTypes.shape({
+    trackName: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
+  }).isRequired,
 };
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,12 +2,12 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
-import Loading from './Loading';
 import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
   state = {
     musica: [],
+    test: [],
   };
 
   componentDidMount() {
@@ -23,22 +23,23 @@ export default class Album extends Component {
 
     this.setState({
       musica: music,
+      test: music[0],
     });
   };
 
   render() {
-    const { musica } = this.state;
+    const { musica, test } = this.state;
+    const slice = musica.slice(1);
     return (
       <div data-testid="page-album">
         <Header />
-        {musica.length === 0
-          ? <Loading /> : (
-            <div key={ musica[0].artistId }>
-              <p data-testid="artist-name">{musica[0].artistName}</p>
-              <p data-testid="album-name">{musica[0].collectionName}</p>
-              <MusicCard musica={ musica.slice(1) } />
-            </div>
-          ) }
+        <div>
+          <p data-testid="artist-name">{test.artistName}</p>
+          <p data-testid="album-name">{test.collectionName}</p>
+          {slice.map((a) => (
+            <MusicCard key={ a.trackId } musica={ a } />
+          ))}
+        </div>
       </div>
     );
   }
